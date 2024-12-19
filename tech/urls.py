850e6976dabdd99e15c1944cdcd0c47077207091
rafaolivato <rafaolivato82@gmail.com>
2024-12-19 09:42:19 -0300

    Finalizando o aprendizado de trabalhar com o Allauth 2.0

--- tech/urls.py
+++ tech/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 
-from tech.views import index
+from tech.views import index, members
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('members/', members, name='members'),
 
 ]
